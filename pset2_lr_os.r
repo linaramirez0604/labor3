a65fc4f7ebf2d804b1a871a0a78668fdbe7478e0
omstiegman <165583013+omstiegman@users.noreply.github.com>
2024-05-01 19:13:59 -0500

    Add files via upload

--- pset2_lr_os.r
+++ pset2_lr_os.r
@@ -3,13 +3,13 @@
 # Lina Ramirez and Olivia Stiegman
 # 5/1/24
 ##################################
-#install.packages('readstata13')
-#install.packages('cowplot')
+# install.packages('readstata13')
+# install.packages('cowplot')
 
 # Clear environment
 rm(list=ls())
-setwd("/Users/linaramirez/Library/CloudStorage/Dropbox/phd/spring-2024/labor3/pset2")
-
+# setwd("/Users/linaramirez/Library/CloudStorage/Dropbox/phd/spring-2024/labor3/pset2")
+# setwd("/Users/oliviastiegman/Documents/UChicago/2nd Year/Spring Quarter/Topics in Labor Markets/pset2")
 
 # Load packages
 library(gtools)
@@ -20,198 +20,192 @@ library(reshape)
 library(ggplot2)
 library(readstata13)
 library(cowplot)
-
 library(SparseM)
+
 # For testing EM algorithm
-model.mixture.new <-function(nk) {
-  
-  model = list()
-  # model for Y1,Y2,Y3|k 
-  model$A     = array(3*(1 + 0.8*runif(3*nk)),c(3,nk))
-  model$S     = array(1,c(3,nk))
-  model$pk    = rdirichlet(1,rep(1,nk))
-  model$nk    = nk
+model.mixture.new <- function(nk) {
+  model <- list()
+
+  model$A <- array(10 * (1 + 1.5 * runif(3 * nk)), c(3, nk))
+  model$S <- array(1, c(3, nk))
+  model$pk <- rdirichlet(1, rep(1, nk))
+  model$nk <- nk
+
   return(model)
 }
 
 # Generate simulated data
-model.mixture.simulate <-function(model,N,sd.scale) {
-  
-  Y1 = array(0,sum(N)) 
-  Y2 = array(0,sum(N)) 
-  Y3 = array(0,sum(N)) 
-  K  = array(0,sum(N)) 
-  
-  A   = model$A
-  S   = model$S
-  pk  = model$pk
-  nk  = model$nk
+model.mixture.simulate <- function(model, N, sd.scale) {
+  Y1 <- array(0, sum(N))
+  Y2 <- array(0, sum(N))
+  Y3 <- array(0, sum(N))
+  K  <- array(0, sum(N))
+
+  A <- model$A
+  S <- model$S
+  pk <- model$pk
+  nk <- model$nk
 
   # draw K
-  K = sample.int(nk,N,TRUE,pk)
+  K <- sample.int(nk, N, TRUE, pk)
 
   # draw Y1, Y2, Y3
-  Y1  = A[1,K] + S[1,K] * rnorm(N) *sd.scale
-  Y2  = A[2,K] + S[2,K] * rnorm(N) *sd.scale
-  Y3  = A[3,K] + S[3,K] * rnorm(N) *sd.scale
+  Y1 <- A[1, K] + S[1, K] * rnorm(N) * sd.scale
+  Y2 <- A[2, K] + S[2, K] * rnorm(N) * sd.scale
+  Y3 <- A[3, K] + S[3, K] * rnorm(N) * sd.scale
 
-  data.sim = data.table(k=K,y1=Y1,y2=Y2,y3=Y3)
-  return(data.sim)  
+  data.sim <- data.table(k = K, y1 = Y1, y2 = Y2, y3 = Y3)
+
+  return(data.sim)
 }
 
 # Mixture model
 model <- model.mixture.new(3)
-df <- model.mixture.simulate(model,10000,sd.scale=0.5) # simulating with lower sd to see separation
-df_melt <- melt(df,id="k")
-plot <- ggplot(df_melt,aes(x=value,group=k,fill=factor(k))) + geom_density() + facet_grid(~variable) + theme_bw()
+df <- model.mixture.simulate(model, 10000, sd.scale = 0.5)
+df_melt <- melt(df, id = "k")
+plot <- ggplot(df_melt, aes(x = value, group = k, fill = factor(k))) +
+  geom_density() +
+  facet_grid(~variable) +
+  theme_bw()
 print(plot)
 
 ########################################################################################
 #QUESTION 1 AND 2
 
 # Log of a normal pdf with arbitrary mean and variance
-lognormpdf <- function(Y,mu,sigma)  {
-  -0.5 * (  (Y-mu) / sigma )^2   - 0.5 * log(2.0*pi) - log(sigma)  
+lognormpdf <- function(Y, mu, sigma)  {
+  -0.5 * ((Y - mu) / sigma)^2   - 0.5 * log(2.0 * pi) - log(sigma)
 }
 
 # Avoid underflow
 logsumexp <- function(v) {
-  vm = max(v)
-  log(sum(exp(v-vm))) + vm
+  vm <- max(v)
+  log(sum(exp(v - vm))) + vm
 }
 
 # Expectation function
 expectation <- function(N, nk, pk, Y1, Y2, Y3, A, S) {
-    # For the posteriors
-    omega <- array(0,c(N,nk))
-    lpm <- array(0,c(N,nk))
+  # For the posteriors
+  omega <- array(0, c(N, nk))
+  lpm <- array(0, c(N, nk))
 
-    # Initialize the likelihood
-    lik <- 0
+  # Initialize the likelihood
+  lik <- 0
 
-    # Loop over each latent variable type
-    for (i in 1:N) {
-        # Log of the probability of each latent variable type
-        lomega <- log(pk)
+  # Loop over each latent variable type
+  for (i in 1:N) {
+    # Log of the probability of each latent variable type
+    lomega <- log(pk)
 
-        # Log of the probability of each observation given a mean and variance
-        lnorm1 <- lognormpdf(Y1[i], A[1,], S[1,])
-        lnorm2 <- lognormpdf(Y2[i], A[2,], S[2,])
-        lnorm3 <- lognormpdf(Y3[i], A[3,], S[3,])
-        
-        # Calculate log of the likelihood for that latent variable type (also the numerator of the posterior)
-        lall <- lomega + lnorm2 + lnorm1 + lnorm3
-        lpm[i,] <- lall
+    # Log of the probability of each observation given a mean and variance
+    lnorm1 <- lognormpdf(Y1[i], A[1, ], S[1, ])
+    lnorm2 <- lognormpdf(Y2[i], A[2, ], S[2, ])
+    lnorm3 <- lognormpdf(Y3[i], A[3, ], S[3, ])
 
-        # Add to the likelihood
-        lik <- lik + logsumexp(lall)
+    # Calculate log of the likelihood for that latent variable type
+    # (also the numerator of the posterior)
+    lall <- lomega + lnorm2 + lnorm1 + lnorm3
+    lpm[i, ] <- lall
 
-        # Calculate the posterior
-        omega[i,] <- exp(lall - logsumexp(lall))
-    }   
+    # Add to the likelihood
+    lik <- lik + logsumexp(lall)
 
-    return(list(omega=omega, lpm=lpm, lik=lik))
+    # Calculate the posterior
+    omega[i, ] <- exp(lall - logsumexp(lall))
+  }
+
+  return(list(omega = omega, lpm = lpm, lik = lik))
 }
 
 # Maximization function
 maximization <- function(N, nk, omega, Y1, Y2, Y3, A, S) {
-    Y <- as.matrix(c(Y1, Y2, Y3))
-    DY <- as.matrix(kronecker(Y ,rep(1,nk)))
-    d <- as.matrix(kronecker(rep(1, N), diag(nk)))
-    Dkj <- as.matrix(kronecker(diag(3), d))
+  Y <- as.matrix(c(Y1, Y2, Y3))
+  DY <- as.matrix(kronecker(Y ,rep(1,nk)))
+  d <- as.matrix(kronecker(rep(1, N), diag(nk)))
+  Dkj <- as.matrix(kronecker(diag(3), d))
 
-    rw <- as.matrix(c(omega, omega, omega))
-    fit <- lm(DY ~ Dkj - 1, weights = rw)
-    A <- coef(fit)
-    A <- matrix(A, nrow = 3, ncol = nk)
+  rw <- as.matrix(c(omega, omega, omega))
+  fit <- lm(DY ~ Dkj - 1, weights = rw)
+  A <- coef(fit)
+  A <- matrix(A, nrow = 3, ncol = nk)
 
-    resids <- DY - predict(fit)
-    resids_scale <- resids^2 / rw
+  resids <- DY - predict(fit)
+  resids_scale <- resids^2 / rw
 
-    fit_v <- lm(resids_scale ~ Dkj - 1, weights = rw)
-    print(coef(fit_v))
-    S <- sqrt(coef(fit_v))
-    S <- matrix(S, nrow = 3, ncol = nk)
-    
-    return(list(A=A, S=S))
+  fit_v <- lm(resids_scale ~ Dkj - 1, weights = rw)
+  S <- sqrt(coef(fit_v))
+  S <- matrix(S, nrow = 3, ncol = nk)
+
+  return(list(A = A, S = S))
 }
 
 # EM algorithm
 em <- function(N, nk, df, tol) {
 
-    # Initialize variables
-    round <- 1
-    lik_old <- 0
-    check <- Inf
+  # Initialize variables
+  round <- 1
+  lik_old <- 0
+  check <- Inf
 
-    # Initialize parameters
-    pk <- rep(1/nk, nk)
-    A <- matrix(5, nk, nk)
-    S <- matrix(1, nk, nk)
+  # Initialize parameters
+  pk <- rep(1 / nk, nk)
+  A <- matrix(5, 3, nk)
+  S <- matrix(1, 3, nk)
 
-    # Initialize variables for convergence and Q, H functions
-    omega_old <- pk
-    lpm_old <- array(0,c(N,nk))
+  # Initialize variables for convergence and Q, H functions
+  omega_old <- array(pk, c(N, nk))
+  lpm_old <- array(0, c(N, nk))
 
-    # Arrange data
-    Y1 <- df$y1
-    Y2 <- df$y2
-    Y3 <- df$y3
+  # Arrange data
+  Y1 <- df$y1
+  Y2 <- df$y2
+  Y3 <- df$y3
 
-    # Perform EM algorithm until convergence
-    while (check > tol) {
-        print(paste("Round:", round))
+  # Perform EM algorithm until convergence
+  while (check > tol) {
+    print(paste("Round:", round))
 
-        # Expectation step
-        results <- expectation(N, nk, pk, Y1, Y2, Y3, A, S)
+    # Expectation step
+    results <- expectation(N, nk, pk, Y1, Y2, Y3, A, S)
 
-        # Update probabilities of types
-        pk <- colSums(results$omega) / N
- 
-        # Update variables for convergence and Q, H functions
-        lik_new <- results$lik
-        omega_new <- results$omega
-        lpm_new <- results$lpm
+    # Update probabilities of types
+    pk <- colSums(results$omega) / N
 
-        # Maximization step
-        results <- maximization(N, nk, omega_new, Y1, Y2, Y3, A, S)
+    # Update variables for convergence and Q, H functions
+    lik_new <- results$lik
+    omega_new <- results$omega
+    lpm_new <- results$lpm
 
-        # Update means and variances
-        A <- results$A
-        S <- results$S
+    # Maximization step
+    results <- maximization(N, nk, omega_new, Y1, Y2, Y3, A, S)
 
-        # Calculate Q and H functions
-        Q1 <- sum(((omega_old) * lpm_old))
-        Q2 <- sum(((omega_old) * lpm_new))
-        H1 <- - sum((omega_old) * log(omega_old))
-        H2 <- - sum((omega_old) * log(omega_new))
-        
-        print(paste("Q1:", Q1))
-        print(paste("Q2:", Q2))
-        print(paste("H1:", H1))
-        print(paste("H2:", H2))
-        
-        # Calculate convergence criterion
-        check <- abs(lik_new - lik_old)
+    # Update means and variances
+    A <- results$A
+    S <- results$S
 
-        print(paste("Check:", check))
-        print(paste("pk:", pk))
-        print(paste("A:", A))
-        print(paste("S:", S))
+    # Calculate Q and H functions
+    Q1 <- sum(((omega_old) * lpm_old))
+    Q2 <- sum(((omega_old) * lpm_new))
+    H1 <- - sum((omega_old) * log(omega_old))
+    H2 <- - sum((omega_old) * log(omega_new))
 
-        # Update variables for next iteration
-        round <- round + 1
-        lik_old <- lik_new
-        omega_old <- omega_new
-        lpm_old <- lpm_new
-    }
+    # Calculate convergence criterion
+    check <- abs(lik_new - lik_old)
 
-    return(list(pk=pk, A=A, S=S))
+    print(paste("Check:", check))
+
+    # Update variables for next iteration
+    round <- round + 1
+    lik_old <- lik_new
+    omega_old <- omega_new
+    lpm_old <- lpm_new
+  }
+
+  return(list(pk = pk, A = A, S = S))
 
 }
 
 em_results <- em(10000, 3, df, tol=1e-6)
-
 
 ##### QUESTION 3: With simulated data ####
 
@@ -302,27 +296,27 @@ ggsave("./output/pk.pdf")
 
 
 ##### QUESTION 4: Estimating on PSID Data #### 
-
-psid_df = data.table(read.dta13("./AER_2012_1549_data/output/data4estimation.dta"))
+psid_df <- data.table(read.dta13("./AER_2012_1549_data/output/data4estimation.dta"))
 
 #computing the wage residuals 
-
-fit = lm(log(log_y) ~ year + marit + state_st ,psid_df ,na.action=na.exclude)
+fit <- lm(log(log_y) ~ year + marit + state_st, psid_df, na.action = na.exclude)
 psid_df[, log_yr := residuals(fit)]
 
 # extract lags
 setkey(psid_df,person,year)
-psid_df[, log_yr_l1 := psid_df[J(person,year-2),log_yr]]
-psid_df[, log_yr_l2 := psid_df[J(person,year-4),log_yr]]
+psid_df[, log_yr_l1 := psid_df[J(person, year - 2), log_yr]]
+psid_df[, log_yr_l2 := psid_df[J(person, year - 4), log_yr]]
 
 # compute difference from start
-fdata = psid_df[!is.na(log_yr*log_yr_l1*log_yr_l2)][,list(y1=log_yr_l2,y2=log_yr_l1,y3=log_yr)] 
+fdata <- psid_df[!is.na(log_yr * log_yr_l1 * log_yr_l2)][, list(y1 = log_yr_l2, y2 = log_yr_l1, y3 = log_yr)]
 
-K = sample.int(3,4941,TRUE,model$pk)
-K_df <- data.frame(k=K)
+K <- sample.int(3, 4941, TRUE, model$pk)
+K_df <- data.frame(k = K)
 
 fdata <- cbind(fdata, K_df)
 
-#Running the EM: 
-em_results <- em(10000, 3, df, tol=1e-6)
+#Running the EM algorithm
+psid_em3_results <- em(4941, 3, fdata, tol = 1e-3)
+psid_em4_results <- em(4941, 4, fdata, tol = 1e-3)
+psid_em5_results <- em(4941, 5, fdata, tol = 1e-3)
 
